306bd365447ef4be383400a506cd2d9c9a7f8888
Max Audron <audron@cocaine.farm>
2021-10-01 18:19:44 +0200

    remove replaced "Network" derive macro

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -3,11 +3,6 @@ use syn;
 mod from;
 mod network;
 
-#[proc_macro_derive(Network, attributes(network))]
-pub fn network(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    network::network(input)
-}
-
 #[proc_macro_derive(NetworkList, attributes(network))]
 pub fn network_list(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     network::network_list(input)
